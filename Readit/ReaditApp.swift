4765130b80bdb1ceb15b8cfcf3fb783bcd182d95
kubinoes <j.schovanec@protonmail.com>
2023-02-21 14:22:49 +0400

    Initial Commit

---
+++ Readit/ReaditApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReaditApp.swift
+//  Readit
+//
+//  Created by Jakub Schovanec on 21/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReaditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
